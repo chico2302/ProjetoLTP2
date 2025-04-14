4aa11282f20f675fe9876da42c53d6e338a67a39
Francisco Ribeiro <chicobr1234@gmail.com>
2025-04-14 14:52:53 -0300

    Create pacoteViagem.java
    classe p/ incluir pacote de viagem e lista com todos eles 
    - adicionar serviços e suas funções

---
+++ pacoteViagem.java
@@ -0,0 +1,56 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class pacoteViagem {
+	//variaveis 
+	String nome;
+	String destino;
+	int duracao;
+	double preco;
+	String tipo;
+	ArrayList<String> servicos = new ArrayList<String>();
+	static 	ArrayList<pacoteViagem> pacotes = new ArrayList<pacoteViagem>();
+	
+	// construtor
+	public pacoteViagem(String nome, String destino, int duracao, double preco, String tipo) {
+		this.nome = nome;
+		this.destino = destino;
+		this.duracao = duracao;
+		this.preco = preco;
+		this.tipo = tipo;
+		pacotes.add(this);
+
+	}
+	
+	// funcoes relacionadas aos pacotes
+	public void listarPacotes() {
+		for (pacoteViagem p : pacotes) {
+		    System.out.println("Nome: " + p.nome + 
+		                       " | Destino: " + p.destino + 
+		                       " | Duração: " + p.duracao + 
+		                       " | Preço: " + p.preco + 
+		                       " | Tipo: " + p.tipo +
+		                       "----------------------------");
+		}
+		
+	}
+	
+	public void pesquisarPacotes(String nomeProcura) {
+		for (pacoteViagem p : pacotes) {
+			if (p.nome.equals(nomeProcura)) {
+				System.out.println(p.nome + "encontrado!");
+			}
+		}
+	}
+	
+	public void excluirPacote(String nomeExcluir) {
+		for (int i = 0; i < pacotes.size(); i++) {
+	        if (pacotes.get(i).nome.equals(nomeExcluir)) {
+	            System.out.println(pacotes.get(i).nome + " excluído!");
+	            pacotes.remove(i);
+	            return;
+	        }
+		}
+	}
+}
