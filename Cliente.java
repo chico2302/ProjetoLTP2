47a4c8c28c328b8e55e7e1100ae47d32ee63bd3e
Francisco Ribeiro <chicobr1234@gmail.com>
2025-04-23 16:14:27 -0300

    Update Cliente.java
    Muitas alterações, alteramos as estruturas de todos arquivos para incluir as interfaces e os relacionamentos

--- Cliente.java
+++ Cliente.java
@@ -1,60 +1,55 @@
 package classes;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import classes.PacoteViagem;
+import classes.CadastroPacote;
 
 public abstract class Cliente {
 	String nome;
 	int telefone;
 	String email;
+	
+	ArrayList<PacoteViagem> pacoteRelacionado = new ArrayList<PacoteViagem>();
 	static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 	
 	public Cliente(String nome, int telefone, String email) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.email = email;
-		clientes.add(this);
+		CadastroPacote.adicionarCliente(this);
 	}
 	
-	public void Listar() {
-		for (int i = 0; i < clientes.size(); i++) {
-			System.out.println(clientes.get(i).nome);
-		}
+	public void adicionarPacote(PacoteViagem pacote) {
+		pacoteRelacionado.add(pacote);
 	}
-	
-	public void Procurar(String nome){
-		for (int i = 0; i < clientes.size(); i++) {
-			if (clientes.get(i).nome.equals(nome)) {
-				System.out.println("Cliente encontrado! Nome: " + clientes.get(i).nome + " Telefone: " + clientes.get(i).telefone + " Email: " + clientes.get(i).email);
-			}
-			}
-		}
-	
 
-	public void Eliminar(String nomeRemove) {
-		for (int i = 0; i < clientes.size(); i++) {
-			if (clientes.get(i).nome.equals(nomeRemove)) {
-				clientes.remove(i);
-				System.out.println("Nome " + nomeRemove + " removido!");
-			}
-		}
+	public String getNome() {
+		return nome;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public String getEmail() {
+		return email;
 	}
 }
-
 class ClienteNacional extends Cliente {
-	int cpf;
+	protected int cpf;
 	
 	public ClienteNacional(String nome,int telefone, String email, int cpf) {
-	super(nome,telefone,email);
-	this.cpf = cpf;
+		super(nome,telefone,email);
+		this.cpf = cpf;
 	}
 }
-
 class ClienteInternacional extends Cliente {
-	int passaporte;
+	protected int passaporte;
 	
 	public ClienteInternacional(String nome,int telefone, String email, int passaporte) {
-	super(nome,telefone,email);
-	this.passaporte = passaporte;
+		super(nome,telefone,email);
+		this.passaporte = passaporte;
 	}
+	
 }
